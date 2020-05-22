7c33254a3acb1ec3b35a69a8f53946419d9a3e65
Janshai <jack.r.s.adams@gmail.com>
2020-05-22 02:44:53 +0100

    change simulator

--- firebaseCloudFuncs/unitTests.sh
+++ firebaseCloudFuncs/unitTests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../Convoy
-xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=13.2.2' build test | xcpretty
+xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.2.2' build test | xcpretty
