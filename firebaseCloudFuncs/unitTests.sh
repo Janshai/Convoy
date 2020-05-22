ee7857e8a636af079946a68e3c88eeee228e70f5
Janshai <jack.r.s.adams@gmail.com>
2020-05-22 14:33:18 +0100

    add retries

--- firebaseCloudFuncs/unitTests.sh
+++ firebaseCloudFuncs/unitTests.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ../Convoy
-xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.2.2' build test | xcpretty
+n=0
+until [ "$n" -ge 5 ]
+do
+   xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 11,OS=13.2.2' build test | xcpretty -t; test ${PIPESTATUS[0]} -eq 0 && break # substitute your command here
+   n=$((n+1))
+   sleep 15
+done
