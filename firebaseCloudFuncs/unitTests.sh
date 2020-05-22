24ff06548371c23232a4b3a3e2c0c9b6a4d35be2
Janshai <jack.r.s.adams@gmail.com>
2020-05-22 01:56:47 +0100

    fix bash typo

--- firebaseCloudFuncs/unitTests.sh
+++ firebaseCloudFuncs/unitTests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../Convoy
-- xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' build test | xcpretty
+xcodebuild -workspace Convoy.xcworkspace -scheme Travis -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone X,OS=12.1' build test | xcpretty
